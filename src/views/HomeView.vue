5b9024f706a4daa4a38bb10d075da4de12bf2b55
Dolapo~icode <fabowalemuhawwal@gmail.com>
2026-07-03 20:11:20 +0100

    update: home update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,12 +8,17 @@ import FooterSection from '@/components/landing/FooterSection.vue';
 </script>
 
 <template>
-  <main class="hidden lg:block">
-    <HeaderSection />
-    <HeroSection />
-    <PlatformTour />
-    <AiTour />
-    <HexagonPart />
-    <FooterSection />
+  <main class="flex justify-center items-center lg:block lg:justify-normal lg:items-baseline">
+    <div class="hidden lg:block">
+      <HeaderSection />
+      <HeroSection />
+      <PlatformTour />
+      <AiTour />
+      <HexagonPart />
+      <FooterSection />
+    </div>
+    <div class="text-center mt-[50vh] font-medium text-[16px] w-[400px] lg:hidden">
+      View this website on desktop screens.
+    </div>
   </main>
 </template>
